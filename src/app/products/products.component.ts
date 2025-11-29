a162f60b7a3c756936eb45951b6c645ed30525d1
alfaroukaljwely <aryar4321@gmail.com>
2025-11-29 13:42:25 +0200

    وو

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -9,16 +9,79 @@ export class ProductsComponent {
  ProductList: any[];
   constructor() {
     this.ProductList = [
-      { id: 100, name: 'LenovoThinkpad laptop', price: 100000000, quantity: 1, imgURL: 'https://fakeimg.pl/200x100', categoryID: 1 },
-      { id: 200, name: 'Apple MacBook laptop', price: 2007780, quantity: 0, imgURL: 'https://fakeimg.pl/200x100', categoryID: 1 },
-      { id: 300, name: 'Lenovo Tab 2', price: 3000, quantity: 10, imgURL: 'https://fakeimg.pl/200x100', categoryID: 2 },
-      { id: 400, name: 'Samsung Tab', price: 40.5, quantity: 2, imgURL: 'https://fakeimg.pl/200x100', categoryID: 2 },
-      { id: 500, name: 'Smasung Note 10', price: 50000, quantity: 0, imgURL: 'https://fakeimg.pl/200x100', categoryID: 3 },
-      { id: 600, name: 'Samsung S22 Utlra', price: 600, quantity: 1, imgURL: 'https://fakeimg.pl/200x100', categoryID: 3 },
-      { id: 700, name: 'apple S22 Utlra', price: 500, quantity: 4, imgURL: 'https://fakeimg.pl/200x100', categoryID: 4 },
-      { id: 800, name: 'tochiba Utlra', price: 60000, quantity: 14, imgURL: 'https://fakeimg.pl/200x100', categoryID: 4 },
-      { id: 900, name: 'ththt Utlra', price: 60, quantity: 0, imgURL: 'https://fakeimg.pl/200x100', categoryID: 4 },
-    ]
+      {
+        id: 100,
+        name: 'LenovoThinkpad laptop',
+        price: 100000000,
+        quantity: 1,
+        imgURL: '/1299194.jpg',
+        categoryID: 1,
+      },
+      {
+        id: 200,
+        name: 'Apple MacBook laptop',
+        price: 2007780,
+        quantity: 0,
+        imgURL: '/736462.png',
+        categoryID: 1,
+      },
+      {
+        id: 300,
+        name: 'Lenovo Tab 2',
+        price: 3000,
+        quantity: 10,
+        imgURL: '/742320.png',
+        categoryID: 2,
+      },
+      {
+        id: 400,
+        name: 'Samsung Tab',
+        price: 40.5,
+        quantity: 2,
+        imgURL: '/742320.png',
+        categoryID: 2,
+      },
+      {
+        id: 500,
+        name: 'Smasung Note 10',
+        price: 50000,
+        quantity: 0,
+        imgURL: '/1374648.png',
+        categoryID: 3,
+      },
+      {
+        id: 600,
+        name: 'Samsung S22 Utlra',
+        price: 600,
+        quantity: 1,
+        imgURL: '/1299194.jpg',
+        categoryID: 3,
+      },
+      {
+        id: 700,
+        name: 'apple S22 Utlra',
+        price: 500,
+        quantity: 4,
+        imgURL: '/736462.png',
+        categoryID: 4,
+      },
+      {
+        id: 800,
+        name: 'tochiba Utlra',
+        price: 60000,
+        quantity: 14,
+        imgURL: '/1374648.png',
+        categoryID: 4,
+      },
+      {
+        id: 900,
+        name: 'ththt Utlra',
+        price: 60,
+        quantity: 0,
+        imgURL: '/736462.png',
+        categoryID: 4,
+      },
+    ];
   }
   allProduct(): any[] {
     return this.ProductList;
